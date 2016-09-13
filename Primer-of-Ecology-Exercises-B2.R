c8facd3daff0503852d00fc0cc839f9746756ff7
Gcasas90 <gui.casas@gmail.com>
2016-09-12 22:44:23 -0700

    Exercises B2 - Cont
    Exercises from 'Primer of Ecology with R' appendix B section B2

--- Primer-of-Ecology-Exercises-B2.R
+++ Primer-of-Ecology-Exercises-B2.R
@@ -3,3 +3,7 @@
 ##Assigns the result of the operation 2+3 and prints it on the screen
 a <- 2 + 3
 a
+
+##Assigns the value of a+a to the variable b, then prints the values of a+a and of a+b 
+b <- a + a
+a + a; b + a
