d1f11a089163be4e50943deacaa0a1a7dad2a663
Gcasas90 <gui.casas@gmail.com>
2016-09-15 17:57:52 -0700

    Data manipulation exercise 2

--- Day2/Lohmueller/Data_Manipulation_Exercise.R
+++ Day2/Lohmueller/Data_Manipulation_Exercise.R
@@ -17,15 +17,16 @@ compute_chisquare=function(x){
 snpsDataFrame=read.table('hapmap_CEU_r23a_chr2_ld-1.txt',header=TRUE)
 snps=as.matrix(snpsDataFrame)
 
-## Apply chisqure test on each row
+## Perform chisquare analysis and extract pvalues (1a)
 chisqs=apply(snps,1,compute_chisquare)
-
-## Extract the p-values for the tests
 pvals=pchisq(chisqs,1,lower.tail=FALSE)
 
+## Find the number of SNPs analyzed (1c)
 num_pval <- length(pvals)
 
 print(paste(num_pval, "SNPs analyzed on the chi-square test"))
+
+## Print number and proportion of SNPs under 0.05, 0.01 and 0.001 thresholds (1b)
 
 for (p in c(0.05, 0.01, 0.001))
 {
@@ -34,6 +35,8 @@ for (p in c(0.05, 0.01, 0.001))
     print(paste0(Below_Thr, " SNPs have p-value under ", p, "(", proportion*100, "%)"))
 }
 
+## Calculate the expected pvalues (1d)
+
 exp_pvals <- rep(0, num_pval)
 
 for(i in 1:num_pval)
@@ -41,13 +44,24 @@ for(i in 1:num_pval)
     exp_pvals[i] <- i/num_pval
 }
 
+## Sort the observed p-values (1e)
 sort_pvals <- sort(pvals)
 
+## Calculating -log10 of p-values (1f)
 log_sort_pvals <- -log10(sort_pvals)
 log_exp_pvals <- -log10(exp_pvals)
 
+## Find the maximum -log10 p-value for window size adjustments
 max_log_pval <- max(log_exp_pvals, log_sort_pvals)
 
+pdf(file = "QQ_plot.pdf",
+    width = 6,
+    height = 6)
+
+par(mfrow = c(1,1),
+    mar = c(4, 4, 3, 2))
+
+## Plot the QQ-plot (1g)
 plot(log_exp_pvals, log_sort_pvals,
      xlab = "-log10(expected P-value)",
      ylab = "-log10(observed P-value)",
@@ -56,7 +70,40 @@ plot(log_exp_pvals, log_sort_pvals,
      cex.lab = 1.5,
      cex.axis = 1.5)
 
+## Plot the diagonal line (1h)
 abline(b = 1, a = 0,
        col = "red",
        lwd = 3,
        lty = 2)
+
+dev.off()
+
+## Loads the glucose phenotypes into R (2a)
+zz <- read.table("pheno.sim.2014-1.txt", header=TRUE)
+glucose <- zz$glucose_mmolperL
+
+## Prints the 25% and 75% quantiles (2b and 2c)
+print(paste("25% of individuals have glucose levels under", quantile(glucose, 0.25)))
+print(paste("25% of individuals have glucose levels above", quantile(glucose, 0.75)))
+
+## Prints the density plot with the vertical quantile lines (2d)
+pdf(file = "Glucose_density.pdf",
+    width = 6,
+    height = 6)
+
+par(mfrow = c(1,1),
+    mar = c(4, 4, 3, 2))
+
+plot(density(glucose),
+     lwd = 3,
+     xlab = "Glucose levels (mmol/L)",
+     main = "Glucose levels distribution")
+
+abline(v = quantile(glucose, 0.25),
+       lty = 2,
+       lwd = 3)
+abline(v = quantile(glucose, 0.75),
+       lty = 2,
+       lwd = 3)
+
+dev.off()
